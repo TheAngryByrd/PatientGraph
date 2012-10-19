afab56520434af6046f560693d37a9290c3b6641
Jimmy Byrd <jimmybyrd@gmail.com>
2012-10-19 15:18:44 -0400

    Init

---
+++ Source/Chartapp/Chartapp/Client/UserCode/PatientDetail.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Collections.Generic;
+using Microsoft.LightSwitch;
+using Microsoft.LightSwitch.Framework.Client;
+using Microsoft.LightSwitch.Presentation;
+using Microsoft.LightSwitch.Presentation.Extensions;
+
+namespace LightSwitchApplication
+{
+    public partial class PatientDetail
+    {
+        partial void Patient_Loaded(bool succeeded)
+        {
+                        // Write your code here.
+            this.SetDisplayNameFromEntity(this.Patient);
+        }
+
+        partial void Patient_Changed()
+        {
+            // Write your code here.
+            this.SetDisplayNameFromEntity(this.Patient);
+        }
+
+        partial void PatientDetail_Saved()
+        {
+            // Write your code here.
+            this.SetDisplayNameFromEntity(this.Patient);
+        }
+    }
+}
